38130e21697ef6cf26ad24f9adda8b9407ccdf52
Ricardo Herrera <rherrerapetit@gmail.com>
2015-06-16 23:40:48 -0500

    First unit test and url mapping, dummy view

--- tourngen/urls.py
+++ tourngen/urls.py
@@ -1,10 +1,9 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
-urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'tourngen.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
+from tournaments import views
 
-    url(r'^admin/', include(admin.site.urls)),
-)
+urlpatterns = [
+    url(r'^$', views.home_page, name='home'),
+    # url(r'^admin/', include(admin.site.urls)),
+]
